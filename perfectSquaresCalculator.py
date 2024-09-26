4042f79e3a92aa2ae22f2f538ea8786d714dddaf
Omar Abdool <abdoolom@shernet.sheridancollege.ca>
2024-09-26 17:31:24 -0400

    using **2 to improve code

--- perfectSquaresCalculator.py
+++ perfectSquaresCalculator.py
@@ -1,13 +1,13 @@
 
 baseNumber = 1
-perfectSquare = 1
+perfectSquare = baseNumber**2
 
 minimum = int(input("Minimum value:"))
 maximum = int(input("Maximum value:"))
 
 while perfectSquare <= maximum:
 
-    perfectSquare = baseNumber * baseNumber
+    perfectSquare = baseNumber**2
 
     if perfectSquare >= minimum and perfectSquare <= maximum:
         print(perfectSquare)
