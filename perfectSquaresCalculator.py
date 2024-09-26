c2ac22dfcaa963bf299d80bea421a0f277769d4c
Omar Abdool <abdoolom@shernet.sheridancollege.ca>
2024-09-26 17:50:47 -0400

    added counter to calculator

--- perfectSquaresCalculator.py
+++ perfectSquaresCalculator.py
@@ -5,12 +5,17 @@ perfectSquare = baseNumber**2
 minimum = int(input("Minimum value:"))
 maximum = int(input("Maximum value:"))
 
+counter = 0
+
 while perfectSquare <= maximum:
 
     if perfectSquare >= minimum:
+        counter += 1
         print(perfectSquare)
 
     baseNumber += 1
     perfectSquare = baseNumber**2
 
+else:
+    print(f"Number of perfect squares in range: {counter}")
 
